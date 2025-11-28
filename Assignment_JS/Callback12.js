a20957e43c1ebde087ca737cc86dc5c89a78bdae
Anujah <anujapanchariya7@gmail.com>
2025-11-28 12:29:03 +0530

    commit

---
+++ Assignment_JS/Callback12.js
@@ -0,0 +1,9 @@
+function timer(duration, onComplete) {
+  setTimeout(() => {
+    onComplete(`Timer of ${duration} ms finished`);
+  }, duration);
+}
+
+timer(1000, (msg) => {
+  console.log(msg); 
+});
